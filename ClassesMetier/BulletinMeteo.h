9afb002b0d053889d20b0dacbf2410115d55e554
lgaliana <lgaliana@btslivh.eu>
2020-01-16 14:56:16 +0100

    ajout bulletinMeteo

---
+++ ClassesMetier/BulletinMeteo.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   BulletinMeteo.h
+ * Author: snir2g2
+ *
+ * Created on 16 janvier 2020, 14:32
+ */
+
+#ifndef BULLETINMETEO_H
+#define BULLETINMETEO_H
+
+class BulletinMeteo {
+public:
+    BulletinMeteo();
+    BulletinMeteo(const BulletinMeteo& orig);
+    virtual ~BulletinMeteo();
+private:
+
+};
+
+#endif /* BULLETINMETEO_H */
+
